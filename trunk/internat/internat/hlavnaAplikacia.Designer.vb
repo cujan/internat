1954c8433161a4e0e2ba5590c057a70840841ced
holubjan <holubjan@ab7b3133-0989-6eee-c4e0-4e562e94900e>
2011-12-14 21:07:16 +0000

    pridany formular student

--- trunk/internat/internat/hlavnaAplikacia.Designer.vb
+++ trunk/internat/internat/hlavnaAplikacia.Designer.vb
@@ -22,18 +22,44 @@ Partial Class hlavnaAplikacia
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ŠtudentiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ŠtudentiToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(887, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ŠtudentiToolStripMenuItem
+        '
+        Me.ŠtudentiToolStripMenuItem.Name = "ŠtudentiToolStripMenuItem"
+        Me.ŠtudentiToolStripMenuItem.Size = New System.Drawing.Size(63, 20)
+        Me.ŠtudentiToolStripMenuItem.Text = "Študenti"
         '
         'hlavnaAplikacia
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(887, 481)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "hlavnaAplikacia"
         Me.Text = "HlavnaAplikacia"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ŠtudentiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 
 End Class
---
+++ trunk/internat/internat/hlavnaAplikacia.vb
@@ -0,0 +1,10 @@
+﻿Public Class hlavnaAplikacia
+
+    Private Sub hlavnaAplikacia_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ŠtudentiToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ŠtudentiToolStripMenuItem.Click
+        student.Show()
+    End Sub
+End Class
